718c45bcf032d39ee9bd605fddac9cecf22a3b7e
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2022-12-01 15:07:17 +0100

    onebeer page is done

--- src/pages/beerList.js
+++ src/pages/beerList.js
@@ -3,9 +3,16 @@ import getUID from '../util/uidFetcher';
 const BeerList = () => {
     const uid = getUID();
     console.log(uid);
+    let beerlist = [{name: "peter"}, {name: "abraham"}, {name: "constantijn"}, {name: "constantijn"}, {name: "constantijn"}, {name: "constantijn"}, {name: "constantijn"}];
     return (
         <div className="beerbody beerlistbody">
-            <div className="beerlistitem">list item</div>
+            <div className="beerlist">
+            {beerlist.map(item => {
+                return (
+                    <div className="beerlistitem">{item.name}</div>
+                )
+            })}
+            </div>
         </div>
       );
 }
