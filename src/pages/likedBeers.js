b518e0e1bc595303ee5c82208743a4fc8496dbb0
Vincent Wijshoff <vincent.wijshoff@student.kuleuven.be>
2022-12-05 16:15:41 +0100

    fixed star rating at liked beers list, now fully done with styling only data needs to be filled in

--- src/pages/likedBeers.js
+++ src/pages/likedBeers.js
@@ -3,27 +3,24 @@ import getUID from '../util/uidFetcher';
 const LikedBeers = () => {
     const uid = getUID();
     console.log(uid);
-    let beerlist = [{name: "peter"}, {name: "abraham"}, {name: "constantijn"}, {name: "constantijn"}, {name: "constantijn"}, {name: "constantijn"}, {name: "constantijn"}];
+    let beerlist = [{name: "peter", rating: 3}, {name: "abraham", rating: 2}, {name: "constantijn", rating: 3}, {name: "constantijn", rating: 4}, {name: "constantijn", rating: 5}, {name: "constantijn", rating: 3}];
     return (
         <div className="beerbody beerlistbody">
             <div className="beerlist">
-            {beerlist.map(item => {
+            {beerlist.map((item, i) => {
                 return (
                     <div className="beerlistitem topx">
                         <div className="beerlistpicture">picture</div>
-                        <div className="beerlistname">name</div>
+                        <div className="beerlistname">{item.name}</div>
                         <div className="likedislikelist">
-                            <div className="rate">
-                                <input type="radio" id="star5" name="rate" value="5" onClick={() => rating(1, 5, uid)}/>
-                                <label htmlFor="star5" title="text">5 stars</label>
-                                <input type="radio" id="star4" name="rate" value="4" onClick={() => rating(1, 4, uid)}/>
-                                <label htmlFor="star4" title="text">4 stars</label>
-                                <input type="radio" id="star3" name="rate" value="3" onClick={() => rating(1, 3, uid)}/>
-                                <label htmlFor="star3" title="text">3 stars</label>
-                                <input type="radio" id="star2" name="rate" value="2" onClick={() => rating(1, 2, uid)}/>
-                                <label htmlFor="star2" title="text">2 stars</label>
-                                <input type="radio" id="star1" name="rate" value="1" onClick={() => rating(1, 1, uid)} checked="checked"/>
-                                <label htmlFor="star1" title="text">1 star</label>
+                            <div className="rating">
+  
+                                <input type="radio" name={"rating_"+i} value="5" id={"5_"+i} onClick={() => rating(1, 5, uid)} checked={item.rating === 5 ? "true" : ""}/><label htmlFor={"5_"+i}>☆</label>
+                                <input type="radio" name={"rating_"+i} value="4" id={"4_"+i} onClick={() => rating(1, 4, uid)} checked={item.rating === 4 ? "true" : ""}/><label htmlFor={"4_"+i}>☆</label>
+                                <input type="radio" name={"rating_"+i} value="3" id={"3_"+i} onClick={() => rating(1, 3, uid)} checked={item.rating === 3 ? "true" : ""}/><label htmlFor={"3_"+i}>☆</label>
+                                <input type="radio" name={"rating_"+i} value="2" id={"2_"+i} onClick={() => rating(1, 2, uid)} checked={item.rating === 2 ? "true" : ""}/><label htmlFor={"2_"+i}>☆</label>
+                                <input type="radio" name={"rating_"+i} value="1" id={"1_"+i} onClick={() => rating(1, 1, uid)} checked={item.rating === 1 ? "true" : ""}/><label htmlFor={"1_"+i}>☆</label>
+
                             </div>
                         </div>    
                     </div>
