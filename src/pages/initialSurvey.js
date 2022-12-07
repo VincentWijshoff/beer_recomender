f5641ba15e404e230f77a0417c2a7873cfc9d753
Jonas Vanmalcot <jonas.vanmalcot@student.kuleuven.be>
2022-12-07 09:30:11 +0100

    modify init survey

--- src/pages/initialSurvey.js
+++ src/pages/initialSurvey.js
@@ -1,20 +1,52 @@
+//import { event } from 'jquery';
 import getUID from '../util/uidFetcher';
 
 const InitialSurvey = () => {
     const uid = getUID();
     console.log(uid);
+
+    let question = {q: "Which of the following types of beer do you like?", 
+    a: ["Lager", "Strong", "White", "Wheat"]}
+
     return (
         <div>
             <div className="footer">
                 <div className="initialsurveyTitle">Initial Survey</div>
             </div>
             <div className="initialsurveybody">
-                {/* question here */}
-                <div className="questionInitialSurvey">Question</div>
+                <div className="questionInitialSurvey">{question.q}</div>
+
+                {/* <div className="answerlistitem" onDrop={(event)=> drop(event)} onDragOver={(event) => allowDrop(event)} >ANDWER</div> */}
+
+                <div className="answerInitialSurvey">
+                {
+                    question.a.map((answer) => {
+                        return (
+                        // <div className="answerlistitem" draggable="true" onDragStart={(event) => drag(event)} >{answer}</div>
+                        <div className="answerlistitem" >{answer}</div>
+                    )})
+                }
+                </div>
+
+
             </div>
 
         </div>
       );
 }
+
+// function allowDrop(ev) {
+//     ev.preventDefault();
+//   }
+  
+//   function drag(ev) {
+//     ev.dataTransfer.setData("text", ev.target.id);
+//   }
+  
+//   function drop(ev) {
+//     ev.preventDefault();
+//     var data = ev.dataTransfer.getData("text");
+//     ev.target.appendChild(document.getElementById(data));
+//   }
 
 export default InitialSurvey;
