770e4a83a711470a1c2bcc5c2c8df3429f7e9d52
Seppe Verboven <seppe.verboven@student.kuleuven.be>
2022-12-09 10:45:38 +0100

    fix path

--- pythonFiles/getBeerByID.py
+++ pythonFiles/getBeerByID.py
@@ -1,13 +1,13 @@
 import pickle
 import pandas as pd
 # Open saved dict 
-with open('idDict.pkl', 'rb') as f:
+with open('./pythonFiles/idDict.pkl', 'rb') as f:
     loaded_dict = pickle.load(f)
 
 def getBeerByID(beerID:str):
     """ Get beer information for beer with id = beerID"""
     fileNum = loaded_dict[beerID]
-    ratings_data = pd.read_json('./filtered-database/beer_{}.json'.format(str(fileNum))).data
+    ratings_data = pd.read_json('./pythonFiles/filtered-database/beer_{}.json'.format(str(fileNum))).data
     for d in ratings_data:
         if str(d.get('id')) == beerID:
             return d
