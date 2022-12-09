321b55a24e0551ff81c27200c83654118791fb79
Seppe Verboven <seppe.verboven@student.kuleuven.be>
2022-12-09 11:56:46 +0100

    Calculate similarity

--- pythonFiles/userBeerSimilarity.py
+++ pythonFiles/userBeerSimilarity.py
@@ -1,7 +1,30 @@
+from constants import *
 from user import user
 
 
 def getSimilarity(u:user, rating_data):
     """Generate similarity number for user profile and beer"""
-    # TODO
-    return True
+    factor = 0
+
+    organicFactor = u.getOrganicPref()
+    factor += organicFactor if rating_data["isOrganic"] == "Y" else (1-organicFactor)
+
+    factor += u.getGlassWarePref(rating_data["glasswareId"])
+
+    factor += u.getStyleIdPref(rating_data["styleId"])
+
+    factor += u.getServingTempPref(rating_data["servingTemperature"])
+
+    abvFactor = u.getAbvPref()
+    abvDist = abs(abvFactor - float(rating_data["abv"]))/MAX_ABV
+    factor += 1 - abvDist
+
+    ibuFactor = u.getIbuPref()
+    ibuDist = abs(ibuFactor - float(rating_data["ibu"]))/MAX_IBU
+    factor += 1 - ibuDist
+
+    srmFactor = u.getSrmPref()
+    srmDist = abs(srmFactor - float(rating_data["srmId"]))/(MAX_SRM - MIN_SRM)
+    factor += 1 - srmDist
+
+    return factor/7
