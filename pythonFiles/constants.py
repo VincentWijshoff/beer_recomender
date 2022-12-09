ed0bff99d7a0d138c2ac3b3cd6303e09d12b67b5
Seppe Verboven <seppe.verboven@student.kuleuven.be>
2022-12-09 11:56:12 +0100

    Add limits

--- pythonFiles/constants.py
+++ pythonFiles/constants.py
@@ -14,5 +14,11 @@ FILTERED_JSONS = 9
 NEW_DATASET_PATH = 'pythonFiles/filtered-database2'
 IDDICT = './pythonFiles/idDict.pkl'
 
+# General
+MAX_ABV = 19.4
+MAX_IBU = 120
+MAX_SRM = 41
+MIN_SRM = 1
+
 # Recommendation
 THRESH = 0.5
